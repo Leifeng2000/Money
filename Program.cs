259ae94862e5684577b5bd9bf83a1e7178f082b2
Leifeng2000 <leifeng7112@gmail.com>
2024-05-03 15:54:33 +0700

    Up

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter US Dollar amount: ");
+        double usdAmount = double.Parse(Console.ReadLine());
+
+        double exchangeRate = 23000;
+        double vndAmount = usdAmount * exchangeRate;
+
+        Console.WriteLine($"Amount in Vietnam Dong: {vndAmount} VND");
+    }
+}
